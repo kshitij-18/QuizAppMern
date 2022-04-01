ac90b319d23a556a31ef389c9f2a6bcff5ec7784
Kshitij <kshitijnath@gmail.com>
2022-04-02 01:24:24 +0530

    fixed index.js error of wrong import for appwrapper

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './store';
-import AppWrapper from './AppWrapper';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 
