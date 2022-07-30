1676c79b47aa96a53918c76647fa74d5160be08d
Kshitij <kshitijnath@gmail.com>
2022-07-31 02:12:23 +0530

    displayed all quizzes on homepage removed dummy quiz cards and added search bar on homepage

--- frontend/src/Screens/Homepage.js
+++ frontend/src/Screens/Homepage.js
@@ -1,24 +1,25 @@
-import { Typography, Box, Paper, CardContent, Card, CardActions, Button } from '@mui/material'
-import React, {useEffect} from 'react'
+import { Typography, Box, Paper, CardContent, Card, CardActions, Button, Stack } from '@mui/material'
+import React, {useEffect, useState} from 'react'
 import {fetchAllQuizes} from '../actions/quizzes'
 import {useDispatch, useSelector} from 'react-redux'
 import {useLocation} from 'react-router-dom'
-import {Snackbar, Alert} from '@mui/material'
+import {Snackbar, Alert, CircularProgress, Skeleton, Grid, Autocomplete, TextField} from '@mui/material'
 import { setErrors } from '../actions/errors'
 
 
 function Homepage() {
   const dispatch = useDispatch()
-  const {quizzes} = useSelector(state => state.quizzes)
+  const {quizzes = {}, loading, error, errorMsg} = useSelector(state => state.quizzes)
   const {errors} = useSelector(state => state.error)
+  const [cardContentLoading, setCardContentLoading] = useState(false)
   let location = useLocation();
+  console.log(location.key)
   if (location.state !== null){
     let message = location.state.message;
     let severity = location.state.severity;
     location.state = null;
     dispatch(setErrors(message, severity));
   }
-
   useEffect(() => {
     dispatch(fetchAllQuizes())
   },[dispatch])
@@ -26,110 +27,106 @@ function Homepage() {
   return (
     <div style={{ height: "100vh" }}>
       {errors?.map((err) => (
-          <>
-          <Snackbar open={err !== null} key={location.key} color={err?.severity} autoHideDuration={3000}>
+        <>
+          <Snackbar
+            open={err !== null}
+            key={location.key}
+            color={err?.severity}
+            autoHideDuration={3000}
+          >
             <Alert severity={err?.severity} sx={{ width: "100%" }}>
               {err?.error}
             </Alert>
           </Snackbar>
         </>
-      ))
-        
-      }
-      <Box
-        mt={2}
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "space-evenly",
-        }}
-      >
-        <div style={{ height: "200px", width: "300px", flex: 1 }}></div>
-        <div
-          style={{
-            width: "300px",
-            flex: 3.5,
-            display: "flex",
-            flexWrap: "wrap",
-          }}
-        >
-          <Card sx={{ minWidth: 275, margin: "10px" }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                Word of the Day
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="medium">Start Quiz</Button>
-            </CardActions>
-          </Card>
+      ))}
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Box
+            mt={2}
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <div style={{ height: "200px", width: "300px", flex: 1 }}></div>
+            <div
+              style={{
+                width: "300px",
+                flex: 3.5,
+                display: "flex",
+                flexWrap: "wrap",
+              }}
+            >
+              <>
+                <Grid container mt={2} mb={4} spacing={2}>
+                  <Grid item xs={8}>
+                    <Autocomplete
+                      id="free-solo-demo"
+                      freeSolo
+                      disableClearable
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          label="Search"
+                          InputProps={{ ...params.InputProps, type: "search" }}
+                        />
+                      )}
+                      options={[].map(() => {})}
+                    />
+                  </Grid>
 
-          <Card sx={{ minWidth: 275, margin: "10px" }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                Word of the Day
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="medium">Start Quiz</Button>
-            </CardActions>
-          </Card>
-
-          <Card sx={{ minWidth: 275, margin: "10px" }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                Word of the Day
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="medium">Start Quiz</Button>
-            </CardActions>
-          </Card>
-
-          <Card sx={{ minWidth: 275, margin: "10px" }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                Word of the Day
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="medium">Start Quiz</Button>
-            </CardActions>
-          </Card>
-
-          <Card sx={{ minWidth: 275, margin: "10px" }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                Word of the Day
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="medium">Start Quiz</Button>
-            </CardActions>
-          </Card>
-        </div>
-        <div style={{ height: "200px", width: "300px", flex: 1 }}></div>
-      </Box>
+                  <Grid item xs={4}></Grid>
+                </Grid>
+              </>
+              {quizzes?.data?.map(({ name, _id }) => (
+                <Card
+                  sx={{
+                    minWidth: 250,
+                    margin: "10px",
+                    maxHeight: 150,
+                    paddingBottom: 5,
+                  }}
+                  key={_id}
+                >
+                  <CardContent>
+                    {cardContentLoading ? (
+                      <React.Fragment>
+                        <Skeleton
+                          animation="wave"
+                          variant="text"
+                          width={100}
+                          height={40}
+                        />
+                      </React.Fragment>
+                    ) : (
+                      <Typography sx={{ fontSize: 30 }} color="text.primary">
+                        {name}
+                      </Typography>
+                    )}
+                  </CardContent>
+                  <CardActions>
+                    {cardContentLoading ? (
+                      <Skeleton
+                        animation="wave"
+                        variant="text"
+                        width={50}
+                        height={40}
+                      />
+                    ) : (
+                      <Button size="medium">Start Quiz</Button>
+                    )}
+                  </CardActions>
+                </Card>
+              ))}
+            </div>
+            <div style={{ height: "200px", width: "300px", flex: 1 }}></div>
+          </Box>
+        </>
+      )}
     </div>
   );
 }
